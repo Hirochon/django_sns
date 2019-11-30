77dde540375b63f8c7965b728d9d817f4f4441a9
Hiroto Noda <heacet43@gmail.com>
2019-12-01 01:17:24 +0900

    [add] Add code import some modules

--- sns/views/share.py
+++ sns/views/share.py
@@ -1,2 +1,7 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib import message
+
 def share1(request):
 	path

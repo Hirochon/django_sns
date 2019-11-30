505563096c5eef9850b75300bc1130ef2b550191
Hiroto Noda <heacet43@gmail.com>
2019-12-01 01:27:10 +0900

    [add] Add get_public() in index.py

--- sns/views/index.py
+++ sns/views/index.py
@@ -3,5 +3,17 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import message
 
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/admin/login/')
 def index1(request):
-	path
+	# publicのuserを取得
+	(public_user, public_group) = get_public()
+
+	# POST送信時の処理
+
+# publicなUserとGroupを取得する。
+def get_public():
+	public_user = User.objects.filter(username='public').first()
+	public_group = Group.objects.filter(owner=public_user).first()
+	return (public_user, public_group)

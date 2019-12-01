b9472d5efd3df8d54aa8c5f04ffed5c60861d7c2
Hiroto Noda <heacet43@gmail.com>
2019-12-01 18:48:33 +0900

    [add] Add public file

---
+++ sns/views/public.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from ..models import Group
+
+def get_public():
+	public_user = User.objects.filter(username='public').first()
+	public_group = Group.objects.filter(owner=public_user).first()
+	return (public_user, public_group)

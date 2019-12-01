0560273c9aa9b2520169aef29f2b0172ee69ab09
Hiroto Noda <heacet43@gmail.com>
2019-12-01 18:06:36 +0900

    [del] Delete redirect module

--- sns/views/add.py
+++ sns/views/add.py
@@ -3,5 +3,15 @@ from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required
+
+@loogin_required(login_url='/admin/login/')
 def add1(request):
-	path
+	# 追加するUserを取得
+	add_name = request.GET['name']
+	add_user = User.objects.filter(username=add_name).first()
+	# Userが本人だった場合の処理
+	if add_user == request.user:
+		messages.info(request, "自分自身をFriendに追加することはできません。")
+		return redirect(to='/sns')
+	#publicの取得
--- sns/views/groups.py
+++ sns/views/groups.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 

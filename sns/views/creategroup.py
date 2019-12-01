14c062fe23262cdc5dc3a623b5fe8c69f6810101
Hiroto Noda <heacet43@gmail.com>
2019-12-01 18:28:11 +0900

    [del][add] Delete add.html & Add 'creategroup app'

--- sns/views/creategroup.py
+++ sns/views/creategroup.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from ..models import Group
+
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/admin/login/')
 def creategroup1(request):
-	path
+	# Groupを作り、Userとtitleを設定して保存する。
+	gp = Group()
+	gp.owner = request.User
+	gp.title = request.POST['group_name']
+	gp.save()
+	message.info(request, '新しいグループを作成しました。')
+	return redirect(to='/sns/groups')

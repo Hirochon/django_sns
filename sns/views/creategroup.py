fe5edd605f191179dac4dcce4c2bb2dd030d881e
Hiroto Noda <heacet43@gmail.com>
2019-12-01 18:36:06 +0900

    [mod] Modify creategroup .py

--- sns/views/creategroup.py
+++ sns/views/creategroup.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -9,9 +10,14 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/admin/login/')
 def creategroup1(request):
 	# Groupを作り、Userとtitleを設定して保存する。
-	gp = Group()
-	gp.owner = request.User
-	gp.title = request.POST['group_name']
-	gp.save()
-	message.info(request, '新しいグループを作成しました。')
-	return redirect(to='/sns/groups')
+	if request.method == 'POST':
+		gp = Group()
+		gp.owner = request.user
+		gp.title = request.POST['group_name']
+		gp.save()
+		message.info(request, '新しいグループを作成しました。')
+		return redirect(to='/sns/groups')
+		
+	# POSTありきのプログラムを回避
+	else:
+		return redirect(to='/sns/groups')
